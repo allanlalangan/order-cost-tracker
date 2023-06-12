762d9ff3a6b185fdafe1c30aa9fe2581c9e38355
allanlalangan <allanlalangan@gmail.com>
2023-06-12 11:08:46 -0700

    add meta tag to fix zoom

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.className}>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <body className="bg-emerald-900">{children}</body>
     </html>
   );
