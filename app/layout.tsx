cd696f5382ee04780946d1a780252a9a9cdc3e11
allanlalangan <allanlalangan@gmail.com>
2023-06-12 11:11:43 -0700

    update meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.className}>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1"
+      />
       <body className="bg-emerald-900">{children}</body>
     </html>
   );
